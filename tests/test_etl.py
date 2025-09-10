d3a8500af5a233b5b9054a77b9caa8002e27b77f
Vic <vicoftech@gmail.com>
2025-09-10 14:52:09 -0300

    first commit

---
+++ tests/test_etl.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from etl_prefect import group_by_jurisdiccion
+import pandas as pd
+
+
+def test_plot_jurisdiccion():
+    df = pd.DataFrame([
+        {"name": "Alice", "age": 34, "jurisdiccion": "Buenos Aires"},
+        {"name": "Bob", "age": 20, "jurisdiccion": "Cordoba"},
+    ])
+    outputs = group_by_jurisdiccion.fn(df)  # .fn ejecuta la función del task
+    assert 'jurisdiccion' in outputs
